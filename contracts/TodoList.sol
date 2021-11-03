59d29f6e8add890135156b8f9458007a1281af27
juniormaniusis <junior.maniusis@gmail.com>
2021-11-02 23:11:55 -0300

    rodando com configurações diferentes da do vídeo

--- contracts/TodoList.sol
+++ contracts/TodoList.sol
@@ -1,6 +1,21 @@
-pragma solidity ^0.5.0;
-
 contract TodoList {
-    uint public taskCount = 0;
-    
+    uint256 public taskCount = 0;
+
+    struct Task {
+        uint256 id;
+        string content;
+        bool completed;
+    }
+
+    mapping(uint256 => Task) public tasks;
+
+    constructor() public {
+        createTask("auto generated task");
+    }
+
+    function createTask(string memory _content) public {
+        // inscrease the value by 1
+        taskCount++;
+        tasks[taskCount] = Task(taskCount, _content, false);
+    }
 }
